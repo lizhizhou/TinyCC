9fa8c7113ada984899016df80fe231c70822e05e
lizhizhou <lzz@meteroi.com>
2014-04-05 04:32:22 -0700

    update to 0.9.6
    Signed-off-by: lizhizhou <lzz@meteroi.com>

---
+++ Debug/sources.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+OBJS := 
+C_DEPS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+win32/tools \
+win32/lib \
+win32/examples \
+tests \
+lib \
+lib/i386 \
+examples \
+
